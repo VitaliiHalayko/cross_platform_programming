9965b66c88c8c75cb009d9fdf69f411a1ae9b34c
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-09-17 14:39:35 +0300

    Save local changes

--- lab1/lab1/IO.cs
+++ lab1/lab1/IO.cs
@@ -1,51 +1,54 @@
-namespace lab1;
+using System.Runtime.CompilerServices;
 
-public static class IO {
-	private const string inputFilePath = "INPUT.txt";
-	private const string outputFilePath = "OUTPUT.txt";
+[assembly: InternalsVisibleTo("lab1.Tests")]
+namespace lab1{
+	public static class IO {
+		private const string inputFilePath = "INPUT.txt";
+		private const string outputFilePath = "OUTPUT.txt";
 
-	public static (int n, int k) readDataFromFile() 
-	{
-		if (File.Exists(inputFilePath))
-        {
-			// Read all lines from the file
-			string[] lines = File.ReadAllLines(inputFilePath);
-
-			if (lines.Length == 2)
+		public static (int n, int k) readDataFromFile() 
+		{
+			if (File.Exists(inputFilePath))
 			{
-				// Parse the first line as integer 'n' and the second line as integer 'k'
-				try {
-					int n = int.Parse(lines[0]);
-					int k = int.Parse(lines[1]);
+				// Read all lines from the file
+				string[] lines = File.ReadAllLines(inputFilePath);
 
-					return (n, k);
-				} catch (FormatException e) {
-					// If the file contains invalid data, throw an error message
-					throw new IOException("Input data is incorrect! The file must contain exactly 2 int values.");
+				if (lines.Length == 2)
+				{
+					// Parse the first line as integer 'n' and the second line as integer 'k'
+					try {
+						int n = int.Parse(lines[0]);
+						int k = int.Parse(lines[1]);
+
+						return (n, k);
+					} catch (FormatException e) {
+						// If the file contains invalid data, throw an error message
+						throw new IOException("Input data is incorrect! The file must contain exactly 2 int values.");
+					}
+				}
+				else
+				{
+					// If the file does not contain exactly 2 lines, throw an error message
+					throw new IOException("Input data is incorrect! The file must contain exactly 2 lines.");
 				}
 			}
 			else
 			{
-				// If the file does not contain exactly 2 lines, throw an error message
+				// If the file is not found, throw an error message
 				throw new IOException("Input data is incorrect! The file must contain exactly 2 lines.");
 			}
 		}
-		else
+
+		public static void writePermutationToFile(int[] permutation) 
 		{
-			// If the file is not found, throw an error message
-			throw new IOException("Input data is incorrect! The file must contain exactly 2 lines.");
+			// Write the permutation to the file
+			File.WriteAllText(outputFilePath, string.Join(" ", permutation));
 		}
-	}
 
-	public static void writePermutationToFile(int[] permutation) 
-	{
-		// Write the permutation to the file
-		File.WriteAllText(outputFilePath, string.Join(" ", permutation));
-	}
-
-	public static void writeErrorToFile(string errorMessage) 
-	{
-		// Write the error message to the file
-		File.WriteAllText(outputFilePath, errorMessage);
-	}
+		public static void writeErrorToFile(string errorMessage) 
+		{
+			// Write the error message to the file
+			File.WriteAllText(outputFilePath, errorMessage);
+		}
+	}	
 }
--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,26 +1,23 @@
-﻿using lab1;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("lab1.Tests")]
-class Program
-{
-    static void Main(string[] args)
-    {
-		int n, k;
-		try 
+﻿namespace lab1{
+	class Program
+	{
+		static void Main(string[] args)
 		{
-			// Check if the input file exists in the project folder
-			(n, k) = IO.readDataFromFile();
+			int n, k;
+			try 
+			{
+				// Check if the input file exists in the project folder
+				(n, k) = IO.readDataFromFile();
 
-			int[] permutation = Permutation.getPermutation(n, k);
+				int[] permutation = Permutation.getPermutation(n, k);
 
-			IO.writePermutationToFile(permutation);
-		} 
-		catch (Exception e) 
-		{
-			// Catch any exceptions (e.g., file read or parse errors) and display the error message
-			IO.writeErrorToFile(e.Message);
+				IO.writePermutationToFile(permutation);
+			} 
+			catch (Exception e) 
+			{
+				// Catch any exceptions (e.g., file read or parse errors) and display the error message
+				IO.writeErrorToFile(e.Message);
+			}
 		}
 	}
 }
-
